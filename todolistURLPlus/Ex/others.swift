f9113f46abf712ffcae8dea68c5f8b9cd8463a1f
Alvin Tseng <tim960315@gmail.com>
2020-09-04 17:07:56 +0800

    新增鍵盤功能模組：當推出鍵盤時，自動將畫面上移

--- todolistURLPlus/Ex/others.swift
+++ todolistURLPlus/Ex/others.swift
@@ -34,6 +34,26 @@ enum ScreenSize{
         }
     }
 }
-
-
-
+extension UIViewController
+{
+    ///當推出鍵盤時，自動將畫面上移
+    func autoPushView(){
+        let observer = NotificationCenter.default
+        
+        observer.addObserver(self, selector: #selector(pushView), name: UIResponder.keyboardWillShowNotification, object: nil)
+        observer.addObserver(self, selector: #selector(closeView), name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    @objc func pushView(){
+        let animate = UIViewPropertyAnimator(duration: 0.25, curve: .easeInOut) {
+            self.view.center = CGPoint(x: ScreenSize.centerX.value, y: ScreenSize.centerY.value * 0.5)
+        }
+        animate.startAnimation()
+    }
+    @objc func closeView(){
+        let animate = UIViewPropertyAnimator(duration: 0.25, curve: .easeInOut) {
+            self.view.center = CGPoint(x: ScreenSize.centerX.value, y: ScreenSize.centerY.value )
+        }
+        animate.startAnimation()
+    }
+}
