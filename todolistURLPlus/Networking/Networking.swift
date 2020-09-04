50b17300e600f8404f7118f8bd1ab67bcc924913
RayHsu <rayhsu1990@gmail.com>
2020-09-04 09:15:50 +0800

    123

---
+++ todolistURLPlus/Networking/Networking.swift
@@ -0,0 +1,83 @@
+//
+//  Networking.swift
+//  todolistURLPlus
+//
+//  Created by Ray Hsu on 2020/9/3.
+//  Copyright © 2020 Alvin Tseng. All rights reserved.
+//
+
+import Foundation
+
+
+struct HTTPRequest {
+    
+    let baseURL = "http://35.185.131.56:8000/api/"
+    let endpoint:Endpoint
+    var urlString:String { baseURL + endpoint.rawValue }
+    let method: HTTPMethod
+    let parameters: [String : Any]
+    let contentType:ContentType
+    
+    
+    func request()-> URLRequest{
+        
+        let url = URL(string: self.urlString)!
+        var request = URLRequest(url: url)
+        request.httpMethod = method.rawValue
+        
+        request.addValue(contentType.rawValue, forHTTPHeaderField: "Content-Type")
+        
+        request.httpBody = try? JSONSerialization.data(withJSONObject: parameters, options: JSONSerialization.WritingOptions())
+        
+        return request
+    }
+    
+}
+
+
+struct NetworkManager {
+    
+    func sendRequest<T:Codable>(with request: URLRequest, completion: @escaping (Result<T,NetworkError>) -> Void){
+        let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
+            DispatchQueue.main.async{
+                if let error = error {
+                    completion(.failure(.invalidData(error.localizedDescription)))
+                }
+                //
+                guard let response = response as? HTTPURLResponse else {
+                    completion(.failure(.errorResponse("no response")))
+                    return
+                }
+                switch response.statusCode {
+                case 200 ... 299:
+                    if let token = response.allHeaderFields[AnyHashable("userToken")] as? String {
+                        UserToken.shared.updateToken(by: token)
+                    }
+                    print("Successs" , "\(response.statusCode)")
+                case 400:
+                    completion(.failure(.errorResponse("帳號密碼格式錯誤")))
+                    return
+                default:
+                    completion(.failure(.errorResponse("\(response.statusCode)")))
+                    
+                    
+                }
+                //
+                guard let data = data else{
+                    completion(.failure(.invalidData("invalid Data")))
+                    return
+                }
+                do{
+                    let decorder = JSONDecoder()
+                    let decotedData = try decorder.decode(T.self, from: data)
+                    completion(.success(decotedData))
+                }catch{
+                    completion(.failure(.invalidData("decode error")))
+                }
+                
+            }
+        }
+        task.resume()
+        
+    }
+}
