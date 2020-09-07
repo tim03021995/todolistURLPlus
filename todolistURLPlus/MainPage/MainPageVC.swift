5bc3e02d7162d200bc01ee76cf71a782dd22b2e3
Jimmy Chen <jinny1993820@gmail.com>
2020-09-07 17:41:58 +0800

    完成首頁大頭貼、歡迎標籤

--- todolistURLPlus/MainPage/MainPageVC.swift
+++ todolistURLPlus/MainPage/MainPageVC.swift
@@ -7,11 +7,48 @@
 //
 
 import UIKit
+import SnapKit
 
 class MainPageVC: UIViewController {
-
+    ///設置背景
+    let userName = ""
+    let backgroundImage:UIImageView = {
+        return BackGroundFactory.makeImage(type: .background2)
+    }()
+    ///設置頭貼
+    lazy var headImage: UIImageView =
+    {
+        let head = UserImageFactory.makeImageView(size: .small, image: nil)
+        head.backgroundColor = .gray
+        let nvBottom = navigationController?.navigationBar.frame.maxY
+        let space = 15
+        head.center = CGPoint(
+            x: space + Int(head.frame.width * 0.5),
+            y: Int(nvBottom!) + space + Int(head.frame.width * 0.3)
+        )
+        return head
+    }()
+    
+    lazy var welcomeLabel: UILabel =
+    {
+       let label = UILabel()
+        label.frame = CGRect(x: ScreenSize.width.value * 0.05,
+                             y: self.headImage.frame.maxY * 1.05,
+                             width: ScreenSize.width.value * 0.9,
+                             height: self.headImage.frame.height  * 0.8)
+        label.text = "Welcome back \(self.userName)"
+        label.adjustsFontSizeToFitWidth = true
+        label.textAlignment = .center
+        label.font = UIFont.boldSystemFont(ofSize: 30)
+        label.textColor = .white
+        return label
+    }()
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.view.addSubview(backgroundImage)
+        self.view.addSubview(headImage)
+        self.view.addSubview(welcomeLabel)
+        
 
         // Do any additional setup after loading the view.
     }
@@ -26,5 +63,5 @@ class MainPageVC: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
+    }
 
-}
