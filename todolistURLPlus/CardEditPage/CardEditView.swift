647b19ec07a2aff570b4d555b62bac8e30e5d7fe
Alvin Tseng <tim960315@gmail.com>
2020-09-07 10:34:57 +0800

    新增CardEditPage

---
+++ todolistURLPlus/CardEditPage/CardEditView.swift
@@ -0,0 +1,21 @@
+//
+//  CardEditView.swift
+//  todolistURLPlus
+//
+//  Created by Alvin Tseng on 2020/9/7.
+//  Copyright © 2020 Alvin Tseng. All rights reserved.
+//
+
+import UIKit
+
+class CardEditView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
