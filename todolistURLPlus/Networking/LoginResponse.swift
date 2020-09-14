bf201f2d4fc4a82d6f29d9be560e99cba1e07aaa
Jimmy Chen <jinny1993820@gmail.com>
2020-09-14 15:22:35 +0800

    首頁到列表資料串連

---
+++ todolistURLPlus/Networking/LoginResponse.swift
@@ -0,0 +1,27 @@
+//
+//  LoginResponse.swift
+//  todolistURLPlus
+//
+//  Created by Ray Hsu on 2020/9/14.
+//  Copyright © 2020 Alvin Tseng. All rights reserved.
+//
+
+import Foundation
+
+//MARK:- 登入註冊的資料結構
+
+struct LoginInReaponse:Codable{
+    let status:Bool
+    var error:String?
+    var loginData: LoginData?
+    
+    enum CodingKeys: String, CodingKey {
+        case status
+        case error
+        case loginData = "login_data"
+    }
+}
+
+struct LoginData : Codable {
+    var userToken:String
+}
