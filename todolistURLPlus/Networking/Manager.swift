5f2dafcc8b3fd34733e181487caf8396c476b572
RayHsu <rayhsu1990@gmail.com>
2020-09-15 12:56:09 +0800

    network 修改

--- todolistURLPlus/Networking/Manager.swift
+++ todolistURLPlus/Networking/Manager.swift
@@ -13,39 +13,42 @@ struct NetworkManager {
     func sendRequest<T:Codable>(with request: URLRequest, completion: @escaping (Result<T,NetworkError>) -> Void){
         let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
             DispatchQueue.main.async{
-                if error != nil {
-                    completion(.failure(.systemError))
-                }
-                //
-                guard let response = response as? HTTPURLResponse else {
-                    completion(.failure(.noResponse))
+                if error != nil { completion(.failure(.systemError)) }
+                
+                 guard let response = response as? HTTPURLResponse else { completion(.failure(.noResponse))
                     return
                 }
-                switch response.statusCode {
-                case 200 ... 299:
-                    print("Successs" , "Status Code:\(response.statusCode)")
-                case 400:
-                    completion(.failure(.responseError(statusCode: response.statusCode)))
-                default:
-                    break
-                }
-                //
-                guard let data = data else{
-                    completion(.failure(.noData))
+                guard let data = data else { completion(.failure(.noData))
                     return
                 }
-                
-                do{
-                    let decorder = JSONDecoder()
-                    let decotedData = try decorder.decode(T.self, from: data)
-                    completion(.success(decotedData))
-                }catch{
-//                    print(error)
-                    completion(.failure(.decodeError))
-                }
-                
+                self.responseHandler(data: data, response: response, completion: completion)
             }
         }
         task.resume()
     }
+    
+    private func responseHandler<T:Codable>(data:Data, response:HTTPURLResponse, completion:@escaping (Result<T,NetworkError>) -> Void){
+        
+        switch response.statusCode {
+        case 200 ... 299:
+            print("Successs" , "Status Code:\(response.statusCode)")
+        case 400:
+            completion(.failure(.responseError(statusCode: response.statusCode)))
+        case 401:
+            //refresh token
+            break
+        default:
+            break
+        }
+        
+        do{
+            let decotedData = try JSONDecoder().decode(T.self, from: data)
+            completion(.success(decotedData))
+        }catch{
+            completion(.failure(.decodeError))
+        }
+    }
+
+    
 }
+
--- todolistURLPlus/Networking/Request.swift
+++ todolistURLPlus/Networking/Request.swift
@@ -52,4 +52,3 @@ struct HTTPRequest {
 }
 
 
-//TODO  處理response
