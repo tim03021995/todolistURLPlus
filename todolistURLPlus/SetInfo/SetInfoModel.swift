e10efd327acef6e1a4ee957b2a58284951be6a2b
Alvin Tseng <tim960315@gmail.com>
2020-09-16 16:07:47 +0800

    使用者資料更新model

---
+++ todolistURLPlus/SetInfo/SetInfoModel.swift
@@ -0,0 +1,16 @@
+//
+//  SetInfoModel.swift
+//  todolistURLPlus
+//
+//  Created by Alvin Tseng on 2020/9/16.
+//  Copyright © 2020 Alvin Tseng. All rights reserved.
+//
+
+import UIKit
+struct SetInfoModel{
+    var UserName:String
+    var UserImage:UIImage
+}
+class SetInfoModelManerger{
+    
+}
