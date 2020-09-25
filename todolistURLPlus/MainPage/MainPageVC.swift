0da1743e76905cf68bfd2498a21c44c51a2d7b18
Jimmy Chen <jinny1993820@gmail.com>
2020-09-25 16:51:51 +0800

    修正首頁跳轉使用者資訊頁時，email資料錯誤的問題

--- todolistURLPlus/MainPage/MainPageVC.swift
+++ todolistURLPlus/MainPage/MainPageVC.swift
@@ -489,7 +489,7 @@ class MainPageVC: UIViewController,UICollectionViewDelegate,UICollectionViewData
     @objc func tapToProfileSetting()
     {
         #warning("需要輸入 email")
-        let vc = UserInfoVC(email: "test@test.com")
+        let vc = UserInfoVC(email: userData.email)
         let nc = UINavigationController(rootViewController: vc)
         //       vc.modalPresentationStyle = .overCurrentContext
         present(nc, animated: true, completion: nil)
