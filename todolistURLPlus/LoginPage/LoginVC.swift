54593057438a363df80c56db94eb08ad1cfd5794
Alvin Tseng <tim960315@gmail.com>
2020-10-05 15:38:13 +0800

    新增使用者名稱正則

--- todolistURLPlus/LoginPage/LoginVC.swift
+++ todolistURLPlus/LoginPage/LoginVC.swift
@@ -54,8 +54,8 @@ class LoginVC: UIViewController, Storyboarded {
     }
     
     override func viewWillAppear(_ animated: Bool) {
-        accountTF.text = "2test@test.com"
-        passwordTF.text = "test12345"
+        accountTF.text = "alvin@gmail.com"
+        passwordTF.text = "a00000000"
     }
     //MARK:- Functions
   
